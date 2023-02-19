095b028a0b4bf09939eb60c337d210a85d319560
Ashish Parmar <parmarashish1105@gmail.com>
2023-02-19 10:39:42 +0530

    loading animatin in room

--- Front-end/src/Component/Rooms/Room.js
+++ Front-end/src/Component/Rooms/Room.js
@@ -5,17 +5,22 @@ import room from '../Images/room.png';
 const Room = () => {
 
   const [rooms, setrooms] = useState([]);
+  const [loading, setloading] = useState([]);
+  const [error, seterror] = useState([]);
 
   const getRoom = async () => {
-
+    
+    setloading(true)
     const response = await fetch('/getallrooms');
     const data = await response.json();
     setrooms(data)
+    setloading(false)
   }
 
   useEffect(() => {
-
+    seterror(true)
     getRoom();
+    seterror(false)
 
   }, [])
 
@@ -29,25 +34,28 @@ const Room = () => {
         <div className='row'>
 
           {
-            rooms.map((room) => {
+            loading ? (<h1>Loading..</h1>) : error ? (<h1>Error</h1>) : (rooms.map((room) => {
               return (
                 <>
                   <div className={roomcss.card}>
-                    <img src={room.imageurls[0]} className={roomcss.cardimg}alt="room" />
+                    <img src={room.imageurls[0]} className={roomcss.cardimg} alt="room" />
                     <div className="card-body">
-                      <h5 className="card-title">{room.name}</h5>
+                      <h5 className={roomcss.title}>{room.name}</h5>
                       <p className="card-text">{room.description}</p>
                       <p>Type: {room.type}</p>
                       <p>Maxcount: {room.maxcount}</p>
                       <p>phone: {room.phonenumber}</p>
-                     
+                      <p className={roomcss.detail}>More Detail</p>
                     </div>
                   </div>
 
+
                 </>
               )
-            })
+            }))
+
           }
+
         </div>
       </div>
 
