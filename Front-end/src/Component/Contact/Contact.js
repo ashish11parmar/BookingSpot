f0bdabf144d944f7780d7e249321cc94c36b3af5
Ladani Chelsi <chelsiladani@gmail.com>
2023-02-20 23:40:22 +0530

    about page
    about page design

--- Front-end/src/Component/Contact/Contact.js
+++ Front-end/src/Component/Contact/Contact.js
@@ -5,89 +5,38 @@ const Contact = () => {
   return (
     <>
       {/* parent class  */}
-      <div
-        class="parent"
-        style={{
-          border: "1px solid black",
-          margin: "1rem",
-          padding: "2rem 2rem",
-          textAlign: "center",
-        }}
-      >
+      <div className="parent" style={{margin: "120px",padding: "2rem 2rem",textAlign: "center",}}>
+
+
         {/* child class for contact */}
-        <div
-          class="child"
-          style={{
-            display: "inline-block",
-            border: "1px solid red",
-            padding: "1rem 1rem",
-            verticalAlign: "middle",
-          }}
-        >
+        <div className="child"  style={{ display: "inline-block",verticalAlign: "middle",textAlign:'left', float:'left',marginLeft:'150px' }}>
           <p className="h1 fw-bold">Contact Info</p>
           <p className="mt-3">
             Get in Touch&nbsp;-&nbsp;We're Always Happy To Hear For You
           </p>
+          <p className="mt-3">Address&nbsp;:&nbsp;Rajkot-Gujarat</p>
           <p className="mt-3">Phone&nbsp;:&nbsp;+91&nbsp;&nbsp;7878953245</p>
           <p className="mt-3">Email&nbsp;:&nbsp;booking.spot@gmail.com</p>
-          <p className="mt-3">Address&nbsp;:&nbsp;Rajkot-Gujarat</p>
         </div>
+
 
         {/* child class for form */}
 
-        <div
-          className="child mx-5"
-          style={{
-            display: "inline-block",
-            border: "1px solid red",
-            padding: "1rem 1rem",
-            verticalAlign: "middle",
-          }}
-        >
-          <form>
-            <div class="form-group">
-              <label for="exampleFormControlInput1">Email address</label>
-              <input
-                type="email"
-                class="form-control"
-                id="exampleFormControlInput1"
-                placeholder="name@example.com"
-              />
+        <div className="child mx-5" style={{display: "inline-block", verticalAlign: "middle"}}>
+          <form method="POST">
+            <div class="form-group mt-3" style={{display: "inline-block", verticalAlign: "middle",}}>
+              <input type="name" className="form-control" id="name" name="name" placeholder="Enter Your Name"/>
             </div>
-            <div class="form-group">
-              <label for="exampleFormControlSelect1">Example select</label>
-              <select class="form-control" id="exampleFormControlSelect1">
-                <option>1</option>
-                <option>2</option>
-                <option>3</option>
-                <option>4</option>
-                <option>5</option>
-              </select>
+
+            <div className="form-group mt-3 mx-3" style={{display: "inline-block", verticalAlign: "middle",}}>
+              <input type="email" className="form-control" id="email" name="email" placeholder="Enter Your Email"/>
             </div>
-            <div class="form-group">
-              <label for="exampleFormControlSelect2">
-                Example multiple select
-              </label>
-              <select
-                multiple
-                class="form-control"
-                id="exampleFormControlSelect2"
-              >
-                <option>1</option>
-                <option>2</option>
-                <option>3</option>
-                <option>4</option>
-                <option>5</option>
-              </select>
+          
+            <div className="form-group mt-4">
+              <textarea className="form-control" id="textarea" placeholder="Enter Your Text Here" style={{border:'0.5px solid black', borderRadius:'none',height:'100px'}}/>
             </div>
-            <div class="form-group">
-              <label for="exampleFormControlTextarea1">Example textarea</label>
-              <textarea
-                class="form-control"
-                id="exampleFormControlTextarea1"
-                rows="3"
-              ></textarea>
-            </div>
+
+            <button type="submit" className="btn btn-primary mt-3">Submit</button>
           </form>
         </div>
       </div>
