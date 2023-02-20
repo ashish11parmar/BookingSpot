c5584cbf5ae86ffbde4adbe5c6e6858fbfd16b95
Ladani Chelsi <chelsiladani@gmail.com>
2023-02-20 09:12:55 +0530

    contact page

--- Front-end/src/Component/Contact/Contact.js
+++ Front-end/src/Component/Contact/Contact.js
@@ -1,39 +1,98 @@
-import React from 'react'
-import './Contact.css';
+import React from "react";
+import "../Contact/Contact.module.css";
 
 const Contact = () => {
   return (
     <>
-   <div className='Contact mt-5 mx-4'>
-    <div className='left'>
-    <p className="h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">Contact Info</p>
-    <p className='mt-3'>Get in Touch&nbsp;-&nbsp;We're Always Happy To Hear For You</p>
-    <p className='mt-3'>Phone&nbsp;:&nbsp;+91&nbsp;&nbsp;7878953245</p>
-    <p className='mt-3'>Email&nbsp;:&nbsp;booking.spot@gmail.com</p>
-    <p className='mt-3'>Address&nbsp;:&nbsp;Rajkot-Gujarat</p>
-    </div>
-   </div>
+      {/* parent class  */}
+      <div
+        class="parent"
+        style={{
+          border: "1px solid black",
+          margin: "1rem",
+          padding: "2rem 2rem",
+          textAlign: "center",
+        }}
+      >
+        {/* child class for contact */}
+        <div
+          class="child"
+          style={{
+            display: "inline-block",
+            border: "1px solid red",
+            padding: "1rem 1rem",
+            verticalAlign: "middle",
+          }}
+        >
+          <p className="h1 fw-bold">Contact Info</p>
+          <p className="mt-3">
+            Get in Touch&nbsp;-&nbsp;We're Always Happy To Hear For You
+          </p>
+          <p className="mt-3">Phone&nbsp;:&nbsp;+91&nbsp;&nbsp;7878953245</p>
+          <p className="mt-3">Email&nbsp;:&nbsp;booking.spot@gmail.com</p>
+          <p className="mt-3">Address&nbsp;:&nbsp;Rajkot-Gujarat</p>
+        </div>
 
-   <div className='Form '>
-    <div className='right'>
-      <form method='POST' className=''>
-      <div className="d-flex col lg-6 mb-4">
-        <div className="form-group flex-fill mb-0">
-        <input type="email" id="email" name='email'  placeholder='Enter Your Name' className="form-control" />
+        {/* child class for form */}
+
+        <div
+          className="child mx-5"
+          style={{
+            display: "inline-block",
+            border: "1px solid red",
+            padding: "1rem 1rem",
+            verticalAlign: "middle",
+          }}
+        >
+          <form>
+            <div class="form-group">
+              <label for="exampleFormControlInput1">Email address</label>
+              <input
+                type="email"
+                class="form-control"
+                id="exampleFormControlInput1"
+                placeholder="name@example.com"
+              />
+            </div>
+            <div class="form-group">
+              <label for="exampleFormControlSelect1">Example select</label>
+              <select class="form-control" id="exampleFormControlSelect1">
+                <option>1</option>
+                <option>2</option>
+                <option>3</option>
+                <option>4</option>
+                <option>5</option>
+              </select>
+            </div>
+            <div class="form-group">
+              <label for="exampleFormControlSelect2">
+                Example multiple select
+              </label>
+              <select
+                multiple
+                class="form-control"
+                id="exampleFormControlSelect2"
+              >
+                <option>1</option>
+                <option>2</option>
+                <option>3</option>
+                <option>4</option>
+                <option>5</option>
+              </select>
+            </div>
+            <div class="form-group">
+              <label for="exampleFormControlTextarea1">Example textarea</label>
+              <textarea
+                class="form-control"
+                id="exampleFormControlTextarea1"
+                rows="3"
+              ></textarea>
+            </div>
+          </form>
         </div>
-        </div>
-        <div className='col lg-6 mb-4'>
-          <input type="name" id="name" name='name' placeholder='Enter Your Name' className='forn-control'/>
-        </div>
-        <div className='col lg-6 mb-4'>
-          <input type="email" id="email" name='email' placeholder='Enter Your Email' className='forn-control'/>
-        </div>
-       <textarea type="text" row='5' id="text" placeholder='Enter Your Text Here' className='form-control'></textarea>
-      </form>
-    </div>
-   </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
